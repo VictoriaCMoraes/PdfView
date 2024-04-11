75a542dfd1e9ba2f37d60554efc79d26e38522d9
adena-welt <vchristina122@gmail.com>
2024-04-11 17:57:39 -0300

    correções

--- app/src/main/java/com/adenabrasil/pdfview/MainActivity.java
+++ app/src/main/java/com/adenabrasil/pdfview/MainActivity.java
@@ -1,16 +1,24 @@
 package com.adenabrasil.pdfview;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.net.Uri;
-import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
     public Button buttonOpenPdf;
+
+    private final ActivityResultLauncher<String> mGetContent = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            uri -> {
+                if (uri != null) {
+                    Intent intent = new Intent(MainActivity.this, Pdf.class);
+                    intent.putExtra("pdfUri", uri.toString());
+                    startActivity(intent);
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,27 +27,6 @@ public class MainActivity extends AppCompatActivity {
 
         buttonOpenPdf = findViewById(R.id.buttonOpenPdf);
 
-        buttonOpenPdf.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("application/pdf");
-                startActivityForResult(intent, 42);
-            }
-        });
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
-        if (requestCode == 42 && resultCode == Activity.RESULT_OK) {
-            Uri uri = null;
-            if (resultData != null) {
-                uri = resultData.getData();
-                Intent intent = new Intent(MainActivity.this, Pdf.class);
-                intent.putExtra("pdfUri", uri.toString());
-                startActivity(intent);
-            }
-        }
+        buttonOpenPdf.setOnClickListener(v -> mGetContent.launch("application/pdf"));
     }
 }
--- app/src/main/java/com/adenabrasil/pdfview/Pdf.java
+++ app/src/main/java/com/adenabrasil/pdfview/Pdf.java
@@ -1,48 +1,56 @@
 package com.adenabrasil.pdfview;
 
-import static java.sql.DriverManager.println;
-
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import java.io.IOException;
+import java.io.InputStream;
 import android.net.Uri;
 import android.text.method.ScrollingMovementMethod;
 
 public class Pdf extends AppCompatActivity {
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf);
 
+        TextView textView;
+
         // Referenciando a TextView
         textView = findViewById(R.id.textView);
 
         textView.setMovementMethod(new ScrollingMovementMethod());
 
+        textView.setBackgroundColor(ContextCompat.getColor(this, R.color.beige));
+
         StringBuilder parsedText = new StringBuilder();
+
         try {
             // Carregando o documento PDF
             Uri uri = Uri.parse(getIntent().getStringExtra("pdfUri"));
-            PdfReader reader = new PdfReader(getContentResolver().openInputStream(uri));
+            InputStream inputStream = getContentResolver().openInputStream(uri);
 
-            // Extraindo o texto do PDF
-            int numberOfPages = reader.getNumberOfPages();
-            for (int i = 1; i <= numberOfPages; i++) {
-                String pageText = PdfTextExtractor.getTextFromPage(reader, i); // Extrai o texto da página i
-                // Remove quebras de linha desnecessárias
-                pageText = pageText.replace("\n", " ");
-                pageText = pageText.replace("   ", "\n");
-                parsedText.append(pageText);
+            if (inputStream != null) {
+                PdfReader reader = new PdfReader(inputStream);
+
+                // Extraindo o texto do PDF
+                int numberOfPages = reader.getNumberOfPages();
+                for (int i = 1; i <= numberOfPages; i++) {
+                    String pageText = PdfTextExtractor.getTextFromPage(reader, i); // Extrai o texto da página i
+                    // Remove quebras de linha desnecessárias
+                    pageText = pageText.replace("\n", " ");
+                    pageText = pageText.replace("   ", "\n");
+                    parsedText.append(pageText);
+                }
+                reader.close();
+            } else {
+                Log.e("PdfBox-Android-Sample", "InputStream is null");
             }
-            reader.close();
         } catch (IOException e) {
             Log.e("PdfBox-Android-Sample", "Exception thrown while loading or reading PDF", e);
         }
